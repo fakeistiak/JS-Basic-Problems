ad2519dcd641ca82fbd0be7d00aee8c43643f5d1
fakeistiak <istiakahmedb100.com>
2025-01-02 21:26:04 +0600

    Problem-3 fixed

--- Problem-3.js
+++ Problem-3.js
@@ -1,9 +1,13 @@
-const evenNumbers = [];
+const numbers = [3, 6, 1, 4, 7, 3, 8, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    evenNumbers.push(i);
+for (let i = 0; i < numbers.length - 1; i++) {
+  for (let j = 0; j < numbers.length - 1 - i; j++) {
+    if (numbers[j] > numbers[j + 1]) {
+      let temp = numbers[j];
+      numbers[j] = numbers[j + 1];
+      numbers[j + 1] = temp;
+    }
   }
 }
 
-console.log("Even numbers:", evenNumbers);
+console.log("Sorted numbers:", numbers);
